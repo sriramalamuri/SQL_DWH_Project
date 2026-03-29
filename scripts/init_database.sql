387e2b0596af7fc8a7ef5a5e8a86635987b4afdf
sriramalamuri <sriramvenkata@yahoo.com>
2026-03-29 15:08:45 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,9 @@
+-- In Postgres, directly created a database "DataWarehouse" using shortcut method from the side-bar options
+-- Ensured that the connection is set to the "DataWarehouse" DB
+-- Create Schemas as below in the "DataWarehouse" DB
+
+Create Schema Bronze;
+
+Create Schema Silver;
+
+Create Schema Gold;
